951a3b4c173a4c1081d046f030f6f7995158f096
alexgonse-farmaco <83952268+alexgonse-farmaco@users.noreply.github.com>
2024-01-08 12:56:14 +0100

    Create construct_PRS.sh

---
+++ construct_PRS.sh
@@ -0,0 +1,11 @@
+###For each phenotype, create PRS
+
+module load plink/v2.0
+sample=samplename
+for phenotype in pheno1 pheno2 pheno3;do
+
+plink2 \
+--bfile /farmacologia/home/farmauser/PRS/bfiles/$sample"_OK" \
+--score /farmacologia/home/farmauser/PRS/weights/$phenotype/$phenotype.weightsall 2 4 6 cols=+scoresums \
+--out /farmacologia/home/farmauser/PRS/sample_PRS_output/$sample.$phenotype
+done
