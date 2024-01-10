6f6f6191811fcefd389af8f2c55a890b52aaa2fb
alexgonse-farmaco <83952268+alexgonse-farmaco@users.noreply.github.com>
2024-01-10 08:28:43 +0000

    Update construct_PRS.sh

--- construct_PRS.sh
+++ construct_PRS.sh
@@ -1,9 +1,9 @@
 ###For each phenotype, create sscore files
 
 module load plink/v2.0
-sample=samplename
-for phenotype in pheno1 pheno2 pheno3;do
+sample=example
 
+for phenotype in ADimp ADnrem;do
 plink2 \
 --bfile /farmacologia/home/farmauser/PRS/bfiles/$sample"_OK" \
 --score /farmacologia/home/farmauser/PRS/weights/$phenotype/$phenotype.weightsall 2 4 6 cols=+scoresums \
