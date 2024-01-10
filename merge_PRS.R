947f95fabda0dcf639d86d8db91d80ccadcb3854
alexgonse-farmaco <83952268+alexgonse-farmaco@users.noreply.github.com>
2024-01-10 08:38:48 +0000

    Update merge_PRS.R

--- merge_PRS.R
+++ merge_PRS.R
@@ -2,21 +2,25 @@
 library(data.table)
 library(dplyr)
 
+samplename="example"
 sscorespath="/farmacologia/home/farmauser/PRS/sample_PRS_output/"
 sscores=grep(list.files(path=sscorespath),pattern = ".sscore", value = T)
 
-phenoty=c("pheno1","pneho2")
-filefile=fread("/farmacologia/home/farmauser/PRS/sample_PRS_output/FLX.ADimp.sscore") #one random sscorefile to retrieve IID column
-filefile=filefile%>%dplyr::select(.,IID)
+phenoty=c("ADimp","ADnrem")
+filetoread=paste0("/farmacologia/home/farmauser/PRS/sample_PRS_output/",samplename,".",phenoty[1],".sscore") #one random sscorefile to retrieve IID column
+prslist=fread(filetoread) 
+prslist=prslist%>%dplyr::select(.,IID)
 for (pheno in phenoty){
   phenofile=grep(list.files(path=sscorespath),pattern = pheno, value = T)
   phenofile=grep(phenofile,pattern = ".sscore",value=T)
+  phenofile=grep(phenofile,pattern = samplename,value=T)
   filetemp=paste0(sscorespath,phenofile)
   temp=fread(filetemp)
   temp[[pheno]]=temp$SCORE1_AVG
   temp=temp%>%dplyr::select(.,pheno)
-  filefile=cbind.data.frame(filefile,temp)
+  prslist=cbind.data.frame(prslist,temp)
 }
-colnames(filefile)=c("ID",phenoty)
-filefile[,phenoty]=lapply(phenoty, function(x) scale(filefile[[x]])) #z-score the PRS (mean=0, SD=1)
-fwrite(filefile,"/farmacologia/home/farmauser/PRS/sample_PRS_output/sample_all_PRSCS.txt",sep='\t')
+colnames(prslist)=c("ID",phenoty)
+prslist[,phenoty]=lapply(phenoty, function(x) scale(prslist[[x]])) #z-score the PRS (mean=0, SD=1)
+savefile=paste0("/farmacologia/home/farmauser/PRS/sample_PRS_output/",samplename,"_all_PRSCS.txt")                      
+fwrite(prslist,savefile,sep='\t')
