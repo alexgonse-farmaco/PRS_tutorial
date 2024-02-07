35cfa4e9eff8ac70bafa6cc6608fcaf5d6939852
alexgonse-farmaco <83952268+alexgonse-farmaco@users.noreply.github.com>
2024-02-07 12:45:53 +0000

    Update construct_PRS.sh

--- construct_PRS.sh
+++ construct_PRS.sh
@@ -3,9 +3,21 @@
 module load plink/v2.0
 sample=example
 
-for phenotype in ADimp ADnrem;do
+#for a set of phenotypes
+for phenotype in ADimp ADnrem;do 
 plink2 \
 --bfile /farmacologia/home/farmauser/PRS/bfiles/$sample"_OK" \
 --score /farmacologia/home/farmauser/PRS/weights/$phenotype/$phenotype.weightsall 2 4 6 cols=+scoresums \
 --out /farmacologia/home/farmauser/PRS/sample_PRS_output/$sample.$phenotype
+done
+
+
+#to loop through all possible phenotypes
+dir="/farmacologia/home/farmauser/weights"
+for folder in "$dir"/*/; do
+    phenotype=$(basename "$folder")
+        plink2 \
+    --bfile "/farmacologia/home/farmauser/PRS/bfiles/${sample}_OK" \
+    --score "/farmacologia/home/farmauser/PRS/weights/$phenotype/$phenotype.weightsall" 2 4 6 cols=+scoresums \
+    --out "/farmacologia/home/farmauser/PRS/sample_PRS_output/${sample}.${phenotype}"
 done
